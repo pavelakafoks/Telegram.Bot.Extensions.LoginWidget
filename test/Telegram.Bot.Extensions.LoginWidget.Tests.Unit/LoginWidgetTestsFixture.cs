c815e478f5ea56b0f2fc305d790626d2200dc6ba
PavelAkaFoks <36537908+pavelakafoks@users.noreply.github.com>
2021-09-22 17:33:55 +0300

    Small improvement - ignore empty fields and allow to change allowedTimeOffset value on CheckAuthorization

--- test/Telegram.Bot.Extensions.LoginWidget.Tests.Unit/LoginWidgetTestsFixture.cs
+++ test/Telegram.Bot.Extensions.LoginWidget.Tests.Unit/LoginWidgetTestsFixture.cs
@@ -1,132 +1,126 @@
 using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Security.Cryptography;
-using System.Text;
+using System.Linq;
 
-namespace Telegram.Bot.Extensions.LoginWidget.Tests.Unit
+namespace Telegram.Bot.Extensions.LoginWidget
 {
-    public class LoginWidgetTestsFixture
+    /// <summary>
+    /// A helper class used to verify authorization data
+    /// </summary>
+    public class LoginWidget : IDisposable
     {
-        private const int _testCount = 10;
+        private bool _disposed = false;
+        private readonly HMACSHA256 _hmac;
+        private static readonly DateTime _unixStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-        private static readonly Random _random = new Random();
-
-        public readonly string Token = RandomString();
-
-        public readonly string CurrentTimestamp;
-
-        public const string RealLifeDataTests_Token = "324335643:AAHdDjFRqowmRegO7AHW4PzayNFzkIoMOww";
-        public static readonly SortedDictionary<string, string>[] RealLifeDataTests = new SortedDictionary<string, string>[]
+        /// <summary>
+        /// Construct a new <see cref="LoginWidget"/> instance
+        /// </summary>
+        /// <param name="token">The bot API token used as a secret parameter when checking authorization</param>
+        public LoginWidget(string token)
         {
-            new SortedDictionary<string, string>()
-            {
-                { "id", "168175103" },
-                { "first_name", "Miha" },
-                { "last_name", "Zupan" },
-                { "username", "MihaZupan" },
-                { "photo_url", "https://t.me/i/userpic/320/MihaZupan.jpg" },
-                { "auth_date", "1540852587" },
-                { "hash", "5b108abf4749846e96c4aa449eb65246c500d29cd6711463166bd2ffcf87285f" }
-            },
-            new SortedDictionary<string, string>()
-            {
-                { "id", "168175103" },
-                { "first_name", "Miha" },
-                { "last_name", "Zupan" },
-                { "username", "MihaZupan" },
-                { "photo_url", "https://t.me/i/userpic/320/MihaZupan.jpg" },
-                { "auth_date", "1540852662" },
-                { "hash", "2f917a0cbd0779cc1f06bc089ebc9079dc946818117d5e2e1ebfdcaa9c60d797" }
-            },
-            new SortedDictionary<string, string>()
-            {
-                { "id", "168175103" },
-                { "first_name", "Miha" },
-                { "last_name", "Zupan" },
-                { "username", "MihaZupan" },
-                { "photo_url", "https://t.me/i/userpic/320/MihaZupan.jpg" },
-                { "auth_date", "1540852698" },
-                { "hash", "7855000860fb319cf98c9f26456fd5b9d078d0cfef88997392334be0c1c6b10c" }
-            }
-        };
+            if (token == null) throw new ArgumentNullException(nameof(token));
 
-        public readonly SortedDictionary<string, string>[] ValidTests   = new SortedDictionary<string, string>[_testCount];
-        public readonly SortedDictionary<string, string>[] InvalidTests = new SortedDictionary<string, string>[_testCount];
-
-        public LoginWidgetTestsFixture()
-        {
-            CurrentTimestamp = ((long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds).ToString();
-
-            using (HMACSHA256 hmac = new HMACSHA256())
+            using (SHA256 sha256 = SHA256.Create())
             {
-                using (SHA256 sha256 = SHA256.Create())
-                {
-                    hmac.Key = sha256.ComputeHash(Encoding.ASCII.GetBytes(Token));
-                }
-
-                FillValidData(hmac);
-                FillInvalidData();
+                _hmac = new HMACSHA256(sha256.ComputeHash(Encoding.ASCII.GetBytes(token)));
             }
         }
 
-        private void FillValidData(HMACSHA256 hmac)
+        /// <summary>
+        /// Checks whether the authorization data received from the user is valid
+        /// </summary>
+        /// <param name="fields">A collection containing query string fields as sorted key-value pairs</param>
+        /// <param name="allowedTimeOffset">How old (in seconds) can authorization attempts be to be considered valid (compared to the auth_date field)
+        /// <returns></returns>
+        public Authorization CheckAuthorization(SortedDictionary<string, string> fields, int allowedTimeOffset = 30)
         {
-            for (int i = 0; i < _testCount; i++)
-            {
-                SortedDictionary<string, string> fields = new SortedDictionary<string, string>
-                {
-                    { "auth_date", CurrentTimestamp },
-                    { "id", RandomString() },
-                };
-                fields.Add("hash", ComputeHash(fields, hmac));
+            if (_disposed) throw new ObjectDisposedException(nameof(LoginWidget));
+            if (fields == null) throw new ArgumentNullException(nameof(fields));
+            if (fields.Count < 3) return Authorization.MissingFields;
 
-                ValidTests[i] = fields;
-            }
-        }
+            if (!fields.ContainsKey(Field.Id) ||
+                !fields.TryGetValue(Field.AuthDate, out string authDate) ||
+                !fields.TryGetValue(Field.Hash, out string hash)
+            ) return Authorization.MissingFields;
 
-        private void FillInvalidData()
-        {
-            for (int i = 0; i < _testCount; i++)
-            {
-                // replace field with random data
-                SortedDictionary<string, string> fields = new SortedDictionary<string, string>
-                {
-                    { "auth_date",  CurrentTimestamp },
-                    { "id",         (i % 2) == 0 ? RandomString()   : ValidTests[i]["id"] },
-                    { "hash",       (i % 2) == 1 ? RandomString(64) : ValidTests[i]["hash"] },
-                    { RandomString(), RandomString() }
-                };
+            if (hash.Length != 64) return Authorization.InvalidHash;
 
-                InvalidTests[i] = fields;
-            }
-        }
+            if (!long.TryParse(authDate, out long timestamp))
+                return Authorization.InvalidAuthDateFormat;
 
-        private static string RandomString(int length = 10)
-        {
-            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
-            {
-                byte[] random = new byte[length];
-                rng.GetBytes(random);
-                return Convert.ToBase64String(random).Substring(0, length);
-            }
-        }
-        
-        private static string ComputeHash(SortedDictionary<string, string> fields, HMACSHA256 hmac)
-        {
-            fields.Remove("hash");
+            if (Math.Abs(DateTime.UtcNow.Subtract(_unixStart).TotalSeconds - timestamp) > allowedTimeOffset)
+                return Authorization.TooOld;
+
+            fields.Remove(Field.Hash);
             StringBuilder dataStringBuilder = new StringBuilder(256);
-            foreach (KeyValuePair<string, string> field in fields)
+            foreach (var field in fields)
             {
-                dataStringBuilder.Append(field.Key);
-                dataStringBuilder.Append('=');
-                dataStringBuilder.Append(field.Value);
-                dataStringBuilder.Append('\n');
+                if (!string.IsNullOrEmpty(field.Value))
+                {
+                    dataStringBuilder.Append(field.Key);
+                    dataStringBuilder.Append('=');
+                    dataStringBuilder.Append(field.Value);
+                    dataStringBuilder.Append('\n');
+                }
             }
             dataStringBuilder.Length -= 1; // Remove the last \n
 
-            byte[] signature = hmac.ComputeHash(Encoding.UTF8.GetBytes(dataStringBuilder.ToString()));
+            byte[] signature = _hmac.ComputeHash(Encoding.UTF8.GetBytes(dataStringBuilder.ToString()));
 
-            return BitConverter.ToString(signature).Replace("-", "").ToLower();
+            // Adapted from: https://stackoverflow.com/a/14333437/6845657
+            for (int i = 0; i < signature.Length; i++)
+            {
+                if (hash[i * 2] != 87 + (signature[i] >> 4) + ((((signature[i] >> 4) - 10) >> 31) & -39)) return Authorization.InvalidHash;
+                if (hash[i * 2 + 1] != 87 + (signature[i] & 0xF) + ((((signature[i] & 0xF) - 10) >> 31) & -39)) return Authorization.InvalidHash;
+            }
+
+            return Authorization.Valid;
+        }
+
+        /// <summary>
+        /// Checks whether the authorization data received from the user is valid
+        /// </summary>
+        /// <param name="fields">A collection containing query string fields as key-value pairs</param>
+        /// <returns></returns>
+        public Authorization CheckAuthorization(Dictionary<string, string> fields)
+        {
+            if (fields == null) throw new ArgumentNullException(nameof(fields));
+            return CheckAuthorization(new SortedDictionary<string, string>(fields, StringComparer.Ordinal));
+        }
+
+        /// <summary>
+        /// Checks whether the authorization data received from the user is valid
+        /// </summary>
+        /// <param name="fields">A collection containing query string fields as key-value pairs</param>
+        /// <returns></returns>
+        public Authorization CheckAuthorization(IEnumerable<KeyValuePair<string, string>> fields) =>
+            CheckAuthorization(fields?.ToDictionary(f => f.Key, f => f.Value, StringComparer.Ordinal));
+
+        /// <summary>
+        /// Checks whether the authorization data received from the user is valid
+        /// </summary>
+        /// <param name="fields">A collection containing query string fields as key-value pairs</param>
+        /// <returns></returns>
+        public Authorization CheckAuthorization(IEnumerable<Tuple<string, string>> fields) =>
+            CheckAuthorization(fields?.ToDictionary(f => f.Item1, f => f.Item2, StringComparer.Ordinal));
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+                _hmac?.Dispose();
+            }
+        }
+
+        private static class Field
+        {
+            public const string AuthDate = "auth_date";
+            public const string Id = "id";
+            public const string Hash = "hash";
         }
     }
 }
