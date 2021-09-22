e8ad01ddd9231f4656618331ae3924ece053854c
PavelSamoilenko <pavels@stosolutions.com>
2021-09-22 17:49:53 +0300

    Revert "Small improvement - ignore empty fields and allow to change allowedTimeOffset value on CheckAuthorization"
    This reverts commit 95b8c1d7cf337fd39a94e20f55112724f45a005a.

--- src/Telegram.Bot.Extensions.LoginWidget/WidgetEmbedCodeGenerator.cs
+++ src/Telegram.Bot.Extensions.LoginWidget/WidgetEmbedCodeGenerator.cs
@@ -1,126 +1,66 @@
-using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Linq;
-
 namespace Telegram.Bot.Extensions.LoginWidget
 {
     /// <summary>
-    /// A helper class used to verify authorization data
+    /// Generates JavaScript embed code matching the one found on https://core.telegram.org/widgets/login
     /// </summary>
-    public class LoginWidget : IDisposable
+    public class WidgetEmbedCodeGenerator
     {
-        private bool _disposed = false;
-        private readonly HMACSHA256 _hmac;
-        private static readonly DateTime _unixStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        /// <summary>
+        /// Defaults to 5
+        /// </summary>
+        public static int LoginWidgetJsVersion = 5;
+
+        private WidgetEmbedCodeGenerator() { }
 
         /// <summary>
-        /// Construct a new <see cref="LoginWidget"/> instance
+        /// Generate the embed code that uses a callback function to signal user login
         /// </summary>
-        /// <param name="token">The bot API token used as a secret parameter when checking authorization</param>
-        public LoginWidget(string token)
+        /// <param name="botName">Name of your Telegram bot</param>
+        /// <param name="callbackFunctionName">Name of the callback function (ex. onUserLogin)</param>
+        /// <param name="callbackParameterName">Name of the parameter in the callback function (ex. user -> onUserLogin(user))</param>
+        /// <param name="buttonStyle">Size of the login button</param>
+        /// <param name="showUserPhoto">Show to user photo next to the login button</param>
+        /// <param name="requestAccess">Request access for your bot to message the user</param>
+        /// <returns></returns>
+        public static string GenerateCallbackEmbedCode(
+            string botName,
+            string callbackFunctionName,
+            string callbackParameterName,
+            ButtonStyle buttonStyle = ButtonStyle.Large,
+            bool showUserPhoto = true,
+            bool requestAccess = true)
         {
-            if (token == null) throw new ArgumentNullException(nameof(token));
-
-            using (SHA256 sha256 = SHA256.Create())
-            {
-                _hmac = new HMACSHA256(sha256.ComputeHash(Encoding.ASCII.GetBytes(token)));
-            }
+            return GenerateBaseEmbedCode(botName, buttonStyle, showUserPhoto, requestAccess, string.Concat("data-onauth=\"", callbackFunctionName, "(", callbackParameterName, ")\""));
         }
 
         /// <summary>
-        /// Checks whether the authorization data received from the user is valid
+        /// Generate the embed code that redirects you to the url you specify with parameters in the query string
         /// </summary>
-        /// <param name="fields">A collection containing query string fields as sorted key-value pairs</param>
-        /// <param name="allowedTimeOffset">How old (in seconds) can authorization attempts be to be considered valid (compared to the auth_date field)
+        /// <param name="botName">Name of your Telegram bot</param>
+        /// <param name="redirectUrl">The url to redirect the user to on login</param>
+        /// <param name="buttonStyle">Size of the login button</param>
+        /// <param name="showUserPhoto">Show to user photo next to the login button</param>
+        /// <param name="requestAccess">Request access for your bot to message the user</param>
         /// <returns></returns>
-        public Authorization CheckAuthorization(SortedDictionary<string, string> fields, int allowedTimeOffset = 30)
+        public static string GenerateRedirectEmbedCode(
+            string botName,
+            string redirectUrl,
+            ButtonStyle buttonStyle = ButtonStyle.Large,
+            bool showUserPhoto = true,
+            bool requestAccess = true)
         {
-            if (_disposed) throw new ObjectDisposedException(nameof(LoginWidget));
-            if (fields == null) throw new ArgumentNullException(nameof(fields));
-            if (fields.Count < 3) return Authorization.MissingFields;
-
-            if (!fields.ContainsKey(Field.Id) ||
-                !fields.TryGetValue(Field.AuthDate, out string authDate) ||
-                !fields.TryGetValue(Field.Hash, out string hash)
-            ) return Authorization.MissingFields;
-
-            if (hash.Length != 64) return Authorization.InvalidHash;
-
-            if (!long.TryParse(authDate, out long timestamp))
-                return Authorization.InvalidAuthDateFormat;
-
-            if (Math.Abs(DateTime.UtcNow.Subtract(_unixStart).TotalSeconds - timestamp) > allowedTimeOffset)
-                return Authorization.TooOld;
-
-            fields.Remove(Field.Hash);
-            StringBuilder dataStringBuilder = new StringBuilder(256);
-            foreach (var field in fields)
-            {
-                if (!string.IsNullOrEmpty(field.Value))
-                {
-                    dataStringBuilder.Append(field.Key);
-                    dataStringBuilder.Append('=');
-                    dataStringBuilder.Append(field.Value);
-                    dataStringBuilder.Append('\n');
-                }
-            }
-            dataStringBuilder.Length -= 1; // Remove the last \n
-
-            byte[] signature = _hmac.ComputeHash(Encoding.UTF8.GetBytes(dataStringBuilder.ToString()));
-
-            // Adapted from: https://stackoverflow.com/a/14333437/6845657
-            for (int i = 0; i < signature.Length; i++)
-            {
-                if (hash[i * 2] != 87 + (signature[i] >> 4) + ((((signature[i] >> 4) - 10) >> 31) & -39)) return Authorization.InvalidHash;
-                if (hash[i * 2 + 1] != 87 + (signature[i] & 0xF) + ((((signature[i] & 0xF) - 10) >> 31) & -39)) return Authorization.InvalidHash;
-            }
-
-            return Authorization.Valid;
+            return GenerateBaseEmbedCode(botName, buttonStyle, showUserPhoto, requestAccess, $"data-auth-url=\"{redirectUrl}\"");
         }
 
-        /// <summary>
-        /// Checks whether the authorization data received from the user is valid
-        /// </summary>
-        /// <param name="fields">A collection containing query string fields as key-value pairs</param>
-        /// <returns></returns>
-        public Authorization CheckAuthorization(Dictionary<string, string> fields)
+        private static string GenerateBaseEmbedCode(string botName, ButtonStyle buttonStyle, bool showUserPhoto, bool requestAccess, string data_auth)
         {
-            if (fields == null) throw new ArgumentNullException(nameof(fields));
-            return CheckAuthorization(new SortedDictionary<string, string>(fields, StringComparer.Ordinal));
-        }
-
-        /// <summary>
-        /// Checks whether the authorization data received from the user is valid
-        /// </summary>
-        /// <param name="fields">A collection containing query string fields as key-value pairs</param>
-        /// <returns></returns>
-        public Authorization CheckAuthorization(IEnumerable<KeyValuePair<string, string>> fields) =>
-            CheckAuthorization(fields?.ToDictionary(f => f.Key, f => f.Value, StringComparer.Ordinal));
-
-        /// <summary>
-        /// Checks whether the authorization data received from the user is valid
-        /// </summary>
-        /// <param name="fields">A collection containing query string fields as key-value pairs</param>
-        /// <returns></returns>
-        public Authorization CheckAuthorization(IEnumerable<Tuple<string, string>> fields) =>
-            CheckAuthorization(fields?.ToDictionary(f => f.Item1, f => f.Item2, StringComparer.Ordinal));
-
-        public void Dispose()
-        {
-            if (!_disposed)
-            {
-                _disposed = true;
-                _hmac?.Dispose();
-            }
-        }
-
-        private static class Field
-        {
-            public const string AuthDate = "auth_date";
-            public const string Id = "id";
-            public const string Hash = "hash";
+            return string.Concat(
+                "<script async src=\"https://telegram.org/js/telegram-widget.js?",
+                LoginWidgetJsVersion, "\" data-telegram-login=\"",
+                botName, "\" data-size=\"" + buttonStyle.ToString().ToLower(), "\" ",
+                (showUserPhoto ? "" : "data-userpic=\"false\" "),
+                (requestAccess ? "data-request-access=\"write\" " : ""),
+                data_auth, "></script>");
         }
     }
 }
